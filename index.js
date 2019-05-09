71e4d23d0c716ca013ec0030fc1167e7418c17f9
donna <sheehanqmd@gmail.com>
2019-05-09 16:55:19 -0500

    here we go

---
+++ index.js
@@ -0,0 +1,42 @@
+You should display a list of all the monthly expenses.
+* The list of expenses should be stored in a variable so you can manipulate it more easily.
+* You should be able to add a new bill to your list of expenses using the form.
+* You should be able to remove a bill from your list of expenses using the trash can button next to it.
+* You should calculate the total of all your expenses and display it in the header.
+* This should update anytime a new bill is added.
+* This should updates anytime a bill is deleted.
+
+let expenses = [] 
+
+function myFunction() {
+  var expenses = ["", "", "", ""];
+  bills.toString();
+  document.getElementById("").innerHTML = bills;
+
+}
+
+function calculateBill(amount) {
+    Amount =
+  
+    return amount;
+  }
+
+function(){
+
+}
+function add(element) {
+  var div; 
+  
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < 0; i ++) {
+    div = document.createElement('a');
+    div.innerHTML = '';
+    fragment.appendChild(div);
+  }
+  element.appendChild(fragment);
+}
+
+addBill: function () {
+  $('table td').on('click', function () {
+      $(this).toggleClass('active');
+  });
